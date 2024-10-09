fa7bb4ffd09c29c5975b0a2c97962a87e2894c6a
priyosu <suxpriyo@gmail.com>
2024-10-09 13:27:49 +0600

    added a gap between alert and main contect

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Alert(props) {
   return (
-    props.alert && (
-      <div>
-        <div class={`alert alert-${props.alert.type}`} role="alert">
+    <div style={{ height: `50px` }}>
+      {props.alert && (
+        <div className={`alert alert-${props.alert.type}`} role="alert">
           {props.alert.msg}
         </div>
-      </div>
-    )
+      )}
+    </div>
   );
 }
